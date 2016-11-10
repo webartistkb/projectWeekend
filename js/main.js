63acace0c5070e3faae2d46f1fc72923fbba1dad
Kamal Bhatia <kamal_bhatia@apple.com>
2016-11-09 17:51:27 -0800

    sidebar and list, grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,94 @@
-//Js Goes here
+// Boilerplate code to render 150 apps/items on page
+(function(){
+    for(let i=1; i<=150; i++){
+        var node = document.createElement('LI');
+        node.classList.add('flex-item');
+        var textnode = document.createTextNode('Item ' + i);
+        node.appendChild(textnode);
+        document.getElementById('apps').appendChild(node);
+    }   
+})();
+    
+    function hasClass(element, cls) {
+        return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+    }
+    
+    function changeViews(action,sourceOfAction){
+        console.log(action);
+        if(sourceOfAction === 'keyboard'){
+            action = document.querySelector('button.'+ action);
+            console.log(action);
+        }
+        let active_button = document.querySelector('button.active');
+        active_button.classList.remove('active');
+        action.classList.add('active');
+        var _apps = document.getElementById('apps');
+        if (hasClass(action,'grid')) {
+            _apps.classList.remove("list","metro");
+            _apps.classList.add('grid');
+        }
+        else if(hasClass(action,'list')) {
+            _apps.classList.remove("grid","metro");
+            _apps.classList.add('list');
+        } 
+        else if(hasClass(action,'metro')) {
+            _apps.classList.remove("grid","list");
+            _apps.classList.add('metro');
+        }
+    }
+
+    //TODO: fix event is getting attached to all buttons
+    let sidebarActions = document.getElementsByTagName('button');
+        console.log(typeof(sidebarActions),sidebarActions);
+        for (var i = 0; i < sidebarActions.length; i++) {
+            sidebarActions[i].onclick = function(e){
+                console.log(e);
+                var action = e.target;
+                changeViews(action);
+        }
+        }
+    
+    /* Set the width of the side navigation to 150px */
+    function openSidebar(){
+        console.log('open called');
+        let navBar = document.getElementById("sidenav");
+        navBar.style.width = "80px";
+        navBar.classList.add('open');
+        navBar.classList.remove('closed');
+    }
+    
+    /* Set the width of the side navigation to 0 */
+    function closeSidebar() {
+        console.log('close called');
+        let navBar = document.getElementById("sidenav");
+        navBar.style.width = "0";
+        navBar.classList.add('closed');
+        navBar.classList.remove('open');
+    }
+  
+     document.addEventListener("keydown", function(event) {
+        event.preventDefault();
+        console.log(event);
+         // Fire following events only when sidenav is open
+        let navBar = document.getElementById("sidenav");
+        if(hasClass(navBar,'open')){
+           if (event.keyCode === 16)
+               closeSidebar();
+           else if(event.keyCode === 71) {
+                changeViews('grid','keyboard');
+            }
+           else if(event.keyCode === 76){
+                changeViews('list','keyboard');
+            }
+            else if(event.keyCode === 77){
+                changeViews('metro','keyboard');
+            }
+    } else if(event.keyCode === 16 && hasClass(navBar,'closed')){
+        openSidebar();
+      }                                                 
+});
+
+
+
+ 
+
